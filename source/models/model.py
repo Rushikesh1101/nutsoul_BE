261e615b1ca2c39de89121b12e77c3d6707c1424
Rushikesh1101 <rushikeshpmedhe@gmail.com>
2026-05-08 16:48:22 +0530

    DB changes

--- source/models/model.py
+++ source/models/model.py
@@ -1,14 +1,21 @@
 from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
+import certifi
 
-load_dotenv()
 
 class MongoDB:
     def __init__(self, uri=None, db_name=None):
-        self.uri = uri or os.getenv("MONGO_URI")
-        self.db_name = db_name or os.getenv("MONGO_DB_NAME")
-        self.client = MongoClient(self.uri)
+        self.uri = "mongodb+srv://nutsoulbharat:nutsoul%40999@nutsoul.zumjlyt.mongodb.net/nutsoul?retryWrites=true&w=majority"
+        self.db_name = "NutSoul"
+        self.client = MongoClient(
+            self.uri,
+            tls=True,
+            tlsCAFile=certifi.where(),
+            serverSelectionTimeoutMS=30000,
+            connectTimeoutMS=30000,
+            socketTimeoutMS=30000,
+            retryWrites=True,
+        )
         self.db = self.client[self.db_name]
 
         self.users = self.db["users"]
@@ -16,7 +23,7 @@ class MongoDB:
         self.cart = self.db["cart"]
         self.orders = self.db["orders"]
         self.address = self.db["address"]
-        
+
     def close(self):
         if self.client:
             self.client.close()
